6bfdd141e49b91456b9cd020eaa6039bda59130f
Om Kolekar <omkolekar2712@gmail.com>
2025-08-07 09:27:51 +0530

    com

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-     <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename="/book-view/">
     <App />
   </BrowserRouter>
 );
